9f5e1820350cffe472701cba50a53fc8495342ea
ThaFWord <77698145+ThaFWord@users.noreply.github.com>
2021-01-20 18:31:49 +0000

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+#logicos
+a = 3
+b = 6
+print(a, b)
+#aritmeticos
+c = a + b
+print(c)
+#relacionais
+d = a < b
+print(d)
+#binario
+print(bin(a) + bin(b))
+#identidade
+print(b is a)
+#associacao
+list= [1,2,3,4.5]
+print("5" in list)
+
