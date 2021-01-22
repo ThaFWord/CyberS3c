c79924c5982952a0b76442ef08a925511ad73c0e
ThaFWord <77698145+ThaFWord@users.noreply.github.com>
2021-01-22 16:33:33 +0000

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
 
+file = open("resultado1.txt", "w")
+
+print("For loop" "\n")
+file.write("For loop" '\n')
+
 x = ["windows", "macos", "linux", "solaris", "android", "ios"] 
 for i in x:
     if i != "solaris":
         print(i)
+        file.write(str(i) + '\n')
+        
+file.close()
 
+file = open("resultado1.txt", "a")
+print("\n")
     
-  
+print("While loop" "\n" )
+file.write('\n' + "While loop" + '\n')
+
+
+idx = 0
+while idx < len(x):
+    if x[idx] != "solaris":
+        print(x[idx])
+        file.write(str(x[idx]) + '\n')
+    idx += 1
+   
+file.close()
