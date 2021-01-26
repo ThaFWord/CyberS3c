a953741a43c0f6ed926b5f9085a116f2fe548bf7
ThaFWord <77698145+ThaFWord@users.noreply.github.com>
2021-01-26 19:54:27 +0000

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-import os, datetime, socket, time, threading, sys
+import os, datetime, socket, time, threading, sys, queue
 from concurrent.futures import ThreadPoolExecutor
+
 
 ipAddr = input("Por favor insira um IP valido > ")
 if not os.path.isdir("Scans"):
@@ -9,39 +10,47 @@ if not os.path.isdir("Scans"):
     os.chdir("Scans")
 
 def portScanner(ip, port):
-    try:
-        file = open("portscan.txt", "a")
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        resultado = sock.connect_ex((ip, port))
-        if resultado == 0:
-            print("Port " + str(port) + " is open\n")
-            file.write("Port " + str(port) + " is open\n")
-        else:
-            print("Port " + str(port) + " is closed" )
-        sock.close()
-        file.close()
-    except socket.gaierror:
-        print("Nao foi possivel identificar o endereco.")
-        sys.exit()
-    except socket.error:
-        print("Ocorreu um erro de socket.")
-        sys.exit()
-    except KeyboardInterrupt:
-        print("Scan terminado com sucesso.")
-        sys.exit()
-    except:
-        print("Ocorreu um erro")
-        sys.exit()
+    with threading.Lock():
+        try:
+            file = open("portscan.txt", "a")
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            resultado = sock.connect_ex((ip, port))
+            if resultado == 0:
+                print("A porta " + str(port) + " esta aberta\n")
+                file.write("A porta " + str(port) + " esta aberta\n")
+            else:
+                print("Port " + str(port) + " esta fechada" )
+            sock.close()
+            q.task_done()
+            file.close()
+        except socket.gaierror:
+            print("Nao foi possivel identificar o endereco.")   
+            sys.exit()
+        except socket.error:
+            print("Ocorreu um erro de socket.")
+            sys.exit()
+        except KeyboardInterrupt:
+            print("Scan terminado com sucesso.")
+            sys.exit()
+        except:
+            print("Ocorreu um erro")
+            sys.exit()
+
+q = queue.Queue()
 
 t0 = datetime.datetime.now()
 if __name__ == "__main__":
-    thread_lock = threading.Lock()
     port = 0
-    executer = ThreadPoolExecutor(max_workers=30)
     for port in range(1, 1025):
-        executer.submit(portScanner)
         x = threading.Thread(target=portScanner, args=(ipAddr, port,))
+        x.deamon = True
         x.start()
+        x.join()
+        
+    for item in range(1025):
+        q.put(item)
+        
 t1 = datetime.datetime.now() - t0
 print("Demorou " + str(t1) + " segundos")
 
+q.join()
